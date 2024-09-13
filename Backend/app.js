12c2843890ab13bd28b2527f574e2b142d79700b
Shubham Kumar <63504112+shubhamrozers@users.noreply.github.com>
2024-09-14 01:05:58 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5001; // Changed port
+const PORT = process.env.PORT || 8080; // Changed port
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
